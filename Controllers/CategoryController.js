c516550d6a41cb0fcdc891f9bc9f36ada3bfcf23
dmrfrht <dmrfrht1@gmail.com>
2021-01-23 16:21:31 +0300

    category controlleri tamamladik

--- Controllers/CategoryController.js
+++ Controllers/CategoryController.js
@@ -1,10 +1,50 @@
+const { config } = require('dotenv/types')
 const Category = require('../Models/Category.Model')
-const response = require('../response')
+const Response = require('../response')
 
 exports.list = (req, res) => {
   Category.find({}, (err, categories) => {
-    if (err) new response(null, err).error500(res)
+    if (err) new Response(null, err).error500(res)
 
-    return new response(categories, null).success(res)
+    return new Response(categories, null).success(res)
+  })
+}
+
+exports.getById = (req, res) => {
+  Category.findById(req.params.category_id, (err, category) => {
+    if (err) new Response().notFound(res)
+
+    return new Response(category, null).success(res)
+  })
+}
+
+exports.create = (req, res) => {
+  const category = new Category({ name: req.body.name })
+  category.save((err) => {
+    if (err) new Response(null, err).error500(res)
+
+    return new Response(category, null).created(res)
+  })
+}
+
+exports.update = (req, res) => {
+  Category.findById(req.params.category_id, (err, category) => {
+    if (err) new Response().notFound(res)
+
+    category.name = req.body.name
+    category.save((err) => {
+      if (err) new Response(null, err).error500(res)
+
+      return new Response(category, null).success(res)
+    })
+  })
+}
+
+exports.delete = (req, res) => {
+  Category.findOneAndDelete({ _id: req.params.category_id }, (err, category) => {
+    if (err) new Response(null, err).error500(res)
+    if (!category) new Response().notFound(res)
+
+    return new Response(category, null).success(res)
   })
 }
