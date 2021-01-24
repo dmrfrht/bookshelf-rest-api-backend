662b17f2cff876c518940f886f19bf5d94b8cbb5
dmrfrht <dmrfrht1@gmail.com>
2021-01-24 19:33:44 +0300

    categoryControllerda create ve update endpointleri icin validation kurallarini ekledik

--- Controllers/CategoryController.js
+++ Controllers/CategoryController.js
@@ -1,5 +1,6 @@
 const Category = require('../Models/Category.Model')
 const Response = require('../response')
+const { validationResult } = require('express-validator')
 
 exports.list = (req, res) => {
   Category.find({}, (err, categories) => {
@@ -19,6 +20,9 @@ exports.getById = (req, res) => {
 }
 
 exports.create = (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) new Response(null, errors.array()).error400(res)
+
   const category = new Category({ name: req.body.name })
   category.save((err) => {
     if (err) new Response(null, err).error500(res)
@@ -28,6 +32,9 @@ exports.create = (req, res) => {
 }
 
 exports.update = (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) new Response(null, errors.array()).error400(res)
+
   Category.findById(req.params.category_id, (err, category) => {
     if (err) new Response(null, err).error500(res)
     if (!category) new Response().notFound(res)
--- apiRouter.js
+++ apiRouter.js
@@ -1,20 +1,30 @@
 const router = require('express').Router()
 const CategoryController = require('./Controllers/CategoryController')
 const BookController = require('./Controllers/BookController')
+const { check } = require('express-validator')
 
 // localhost:9090/api/category
-router.route('/category').get(CategoryController.list).post(CategoryController.create)
+router.route('/category')
+  .get(CategoryController.list)
+  .post([check("name").notEmpty().withMessage("İsim alanı boş olamaz")], CategoryController.create)
 
 // localhost:9090/api/category/:category_id
-router.route('/category/:category_id').get(CategoryController.getById).put(CategoryController.update).delete(CategoryController.delete)
+router.route('/category/:category_id')
+  .get(CategoryController.getById)
+  .put([check("name").notEmpty().withMessage("İsim alanı boş olamaz")], CategoryController.update).delete(CategoryController.delete)
 
 // locahost:9090/api/book
-router.route('/book').get(BookController.list).post(BookController.create)
+router.route('/book')
+  .get(BookController.list)
+  .post(BookController.create)
 
 // localhost:9090/api/book/:book_id
-router.route('/book/:book_id').get(BookController.getById).put(BookController.update).delete(BookController.delete)
+router.route('/book/:book_id')
+  .get(BookController.getById)
+  .put(BookController.update).delete(BookController.delete)
 
 // locahost:9000/api/books/:category_id
-router.route('/books/:category_id').get(BookController.listByCategoryId)
+router.route('/books/:category_id')
+  .get(BookController.listByCategoryId)
 
 module.exports = router
